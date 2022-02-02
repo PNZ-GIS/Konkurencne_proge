459c8266ca28490eb226e2d7ab48866e93876e21
PNZ-GIS <miha.blaz@pnz.si>
2022-02-02 14:38:34 +0100

    20220102v3

---
+++ layers/8_LVVariantaDPredorZalogBrezovica_0.js
@@ -0,0 +1,8 @@
+var json_8_LVVariantaDPredorZalogBrezovica_0 = {
+"type": "FeatureCollection",
+"name": "8_LVVariantaDPredorZalogBrezovica_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "qc_id": 0.0, "tipodseka": "3", "length": 9648.1 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 14.44653074686433, 46.029872173695189 ], [ 14.45474741472478, 46.033036916145477 ], [ 14.46466745040334, 46.036489583840343 ], [ 14.54185388584593, 46.05795388484465 ], [ 14.54493830077136, 46.058552866606313 ], [ 14.548036497684921, 46.058815658517247 ], [ 14.550182206542511, 46.05884149489313 ], [ 14.56277367047856, 46.058998447587832 ] ] ] } }
+]
+}
